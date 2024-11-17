7ed0d22fc7d3e737595661842a6a4e0efa4aaf89
Khalid Alharbi <kalharbi@users.noreply.github.com>
2024-11-17 21:55:30 +0300

    multiply the two numbers

--- Main.java
+++ Main.java
@@ -2,8 +2,8 @@ class Main {
     public static void main(String[] args) {
         int first = 10;
         int second = 20;
-        // add two numbers
-        int sum = first + second;
-        System.out.println(first + " + " + second + " = " + sum);
+        // multiply two numbers
+        int result = first * second;
+        System.out.println(first + " * " + second + " = " + result);
     }
 }
