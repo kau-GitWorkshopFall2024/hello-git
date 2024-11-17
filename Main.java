7714ce56b3d2f9f9898f56df98d240b754f7933f
Khalid Alharbi <kalharbi@users.noreply.github.com>
2024-11-17 22:03:41 +0300

    add print line stmt

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ class Main {
     public static void main(String[] args) {
         int first = 10;
         int second = 20;
-       
+        System.out.println("Multiplying two numbers.");
         // multiply two numbers
         int result = first * second;
         System.out.println(first + " * " + second + " = " + result);
