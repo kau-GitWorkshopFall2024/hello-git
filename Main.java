03e928604ece2bb4a018d9a4e0ee33956e13b0cf
m3anv <m3n_osama@hotmail.com>
2024-11-17 21:59:39 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ class Main {
     public static void main(String[] args) {
         int first = 10;
         int second = 20;
-        int third = 30;
+       
         // multiply two numbers
         int result = first * second;
         System.out.println(first + " * " + second + " = " + result);
